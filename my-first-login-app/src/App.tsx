5f55cf95535db17db4ec403b94f46203a3ff6176
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-08-27 21:57:22 +0100

    added react router dom

--- my-first-login-app/src/App.tsx
+++ my-first-login-app/src/App.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
-import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme, Switch } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Signup from "./Signup";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <Signup />
+        {/* <AuthProvider> */}
+          <Switch>
+          < Route path = "/signup" component = {Signup}/>
+          </Switch>
+        {/* </AuthProvider> */}
       </Grid>
     </Box>
   </ChakraProvider>
